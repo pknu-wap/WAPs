6dae90213a388da1c2a8ac91d5075d88aa3632ab
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-10-30 16:37:08 +0900

    feat(ProjectService): Repository를 통해 가져온 데이터를 Response객체로 변환하여 반환하는 메서드 생성

---
+++ server/src/main/java/wap/web2/server/service/ProjectService.java
@@ -0,0 +1,24 @@
+package wap.web2.server.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wap.web2.server.domain.Project;
+import wap.web2.server.payload.response.ProjectResponse;
+import wap.web2.server.repository.ProjectRepository;
+
+@Service
+public class ProjectService {
+
+    ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<ProjectResponse> getProjects(Long year, Long semester) {
+        return projectRepository.findProjectsByYearAndSemester(year, semester)
+            .stream().map(Project::toResponse).toList();
+    }
+}
