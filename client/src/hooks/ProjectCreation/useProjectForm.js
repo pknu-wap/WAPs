6a3fa7688d53d542e5f999a9a1a82b2d0a31767a
yuni <seizethedayunhui@naver.com>
2024-11-01 01:05:42 +0900

    feature : resetForm에 기술스택도 리셋되도록 수정

--- client/src/hooks/ProjectCreation/useProjectForm.js
+++ client/src/hooks/ProjectCreation/useProjectForm.js
@@ -67,19 +67,6 @@ const useProjectForm = () => {
     }));
   };
 
-  // 썸네일 업로드 핸들러
-  // const handleThumbnailUpload = (file) => {
-  //   if (!file.type.startsWith("image/")) {
-  //     setErrorMessage((prev) => ({
-  //       ...prev,
-  //       thumbnail: "이미지 파일만 업로드할 수 있습니다.",
-  //     }));
-  //     return;
-  //   }
-  //   setThumbnail(file);
-  //   setErrorMessage((prev) => ({ ...prev, thumbnail: "" }));
-  // };
-
   // 팀원 이름 입력 포커스 핸들러
   const handleMemberNameFocus = (e, index) => {
     if (index === 0 && !isLeader) {
@@ -188,6 +175,7 @@ const useProjectForm = () => {
     setThumbnail(null);
     setImages([null, null, null, null]);
     setErrorMessage({});
+    setSelectedTechStacks([]);
     setUploadError(null);
   };
 
