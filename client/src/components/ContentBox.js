6ec420ca9d9af993b33124ec8ce7d6cf9cf9ab60
daimlee <daim050822@gmail.com>
2024-11-07 03:28:32 +0900

    fix : 2024를 현재년도로 수정

--- client/src/components/ContentBox.js
+++ client/src/components/ContentBox.js
@@ -5,12 +5,13 @@ import axios from "axios";
 const ContentBox = () => {
   const [projects, setProjects] = useState([]);
   const navigate = useNavigate();
+  const currentYear = new Date().getFullYear(); // 현재 연도 가져오기
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "http://15.164.98.72:8080/project/list?semester=2&projectYear=2024"
+          `http://15.164.98.72:8080/project/list?semester=2&projectYear=${currentYear}`
         );
 
         // 응답 데이터를 콘솔에 출력하여 형식을 확인
@@ -31,7 +32,7 @@ const ContentBox = () => {
     };
 
     fetchData();
-  }, []);
+  }, [currentYear]); // currentYear가 변경될 때마다 fetchData 호출
 
   return (
     <div className="content-box">
