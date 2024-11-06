1fbcf87d9fb5a10c8f0d5de089d07ed5b82d1efd
yuni <seizethedayunhui@naver.com>
2024-11-06 18:16:35 +0900

    refactor : 제대로 라우팅 되도록 App.js 파일 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/menu/Login';
-import Project from './pages/menu/Project';
-import CreatePage from './pages/menu/CreatePage';
-import Vote from './pages/menu/Vote';
-import Map from './pages/menu/Map';
-import MyPage from './pages/menu/MyPage';
-import Web from './pages/project/WebPage';
-import AppPage from './pages/project/AppPage';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/menu/Login";
+import Project from "./pages/menu/Project";
+import CreatePage from "./pages/menu/CreatePage";
+import Vote from "./pages/menu/Vote";
+import Map from "./pages/menu/Map";
+import MyPage from "./pages/menu/MyPage";
+import ProjectDetailPage from "./pages/ProjectDetailPage";
+import AppPage from "./pages/project/AppPage";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <body>
-      <div class="container">
-        <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/CreatePage" element={<CreatePage />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/vote" element={<Vote />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/MyPage" element={<MyPage />} />
+      <body>
+        <div class="container">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/CreatePage" element={<CreatePage />} />
+              <Route path="/project" element={<Project />} />
+              <Route path="/vote" element={<Vote />} />
+              <Route path="/map" element={<Map />} />
+              <Route path="/MyPage" element={<MyPage />} />
 
-          <Route path="/web" element={<Web />} />
-          <Route path="/app" element={<AppPage />} />
-        </Routes>
-      </Router>
-      </div>
-    </body>
-    
+              <Route
+                path="/project/:projectId"
+                element={<ProjectDetailPage />}
+              />
+              <Route path="/app" element={<AppPage />} />
+            </Routes>
+          </Router>
+        </div>
+      </body>
     </>
   );
 }
