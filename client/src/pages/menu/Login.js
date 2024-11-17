c97d9154ebc9c9ec51e9a015e5b07714422e48fc
daimlee <daim050822@gmail.com>
2024-11-18 01:33:22 +0900

    feature : 메인화면으로 이동 버튼 생성
    로그인 버튼 밑에 로그인 없이 접속 버튼 만들고 이 버튼 누르면 메인페이지로 이동

--- client/src/pages/menu/Login.js
+++ client/src/pages/menu/Login.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import '../../assets/Login.css';
-import logo from '../../assets/img/WAP_white_NoBG.png';
-import { isAuthenticated } from '../../utils/auth'; // '../utils/auth'를 '../../utils/auth'로 변경
-
+import React from "react";
+import "../../assets/Login.css";
+import logo from "../../assets/img/WAP_white_NoBG.png";
+import img from "../../assets/img/pngwing.com.png";
+import { useNavigate } from "react-router-dom"; // useNavigate import 추가
 
 const Login = () => {
+  const navigate = useNavigate(); // useNavigate 호출
+
   const handleKakaoLogin = () => {
-    window.location.href = 'http://15.164.98.72:8080/oauth2/authorization/kakao'; // 서버 배포 주소
+    window.location.href = "http://15.164.98.72:8080/oauth2/authorization/kakao"; // 서버 배포 주소
   };
 
-  const handleLoginSuccess = (token) => {
-    localStorage.setItem('token', token); // 토큰 저장
-    window.location.href = '/'; // 홈으로 리다이렉트
+  const handleProceedWithoutLogin = () => {
+    // 로그인 없이 진행
+    navigate("/"); // 메인 화면으로 이동
   };
 
   return (
@@ -19,7 +21,13 @@ const Login = () => {
       <img src={logo} alt="WAP Logo" className="logo-image" />
       <div className="login-buttons">
         <button className="login-button kakao" onClick={handleKakaoLogin}>
-          <span>카카오로 로그인</span>
+          <img src={img} alt="Kakao Logo" className="Kakao-image" /><span>카카오로 로그인</span>
+        </button>
+        <button
+          className="login-button no-login" // 추가 스타일링 클래스
+          onClick={handleProceedWithoutLogin}
+        >
+          <span>로그인 없이 진행</span>
         </button>
       </div>
     </div>
