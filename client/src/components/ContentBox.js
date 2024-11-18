3061bf7d83f6c1372e161a41eb507c5ad474dcce
seizethedayunhui <seizethedayunhui@naver.com>
2024-11-19 00:39:15 +0900

    refactor: https 배포를 위하여, 서버주소를 프록시 서버주소로 변경

--- client/src/components/ContentBox.js
+++ client/src/components/ContentBox.js
@@ -6,13 +6,12 @@ const ContentBox = () => {
   const [projects, setProjects] = useState([]);
   const navigate = useNavigate();
   const currentYear = new Date().getFullYear(); // 현재 연도 가져오기
+  const apiURL = `${process.env.REACT_APP_API_URL_PROXY}/project/list?semester=2&projectYear=${currentYear}`;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          `http://15.164.98.72:8080/project/list?semester=2&projectYear=${currentYear}`
-        );
+        const response = await axios.get(apiURL);
 
         // 응답 데이터를 콘솔에 출력하여 형식을 확인
         console.log("API 응답 데이터:", response.data);
