cceb956be35d3d7f073f8bf1d2e613825e3365e3
seizethedayunhui <seizethedayunhui@naver.com>
2024-11-20 23:40:23 +0900

    feature: 투표페이지에 해당 학기 프로젝트 불러옴

--- client/src/components/Vote/VoteForm.js
+++ client/src/components/Vote/VoteForm.js
@@ -1,29 +1,26 @@
 import React from "react";
 import dogImage from "../../assets/img/dog.png";
-
+import styles from "../../assets/Vote/ProjectVote.module.css";
+import sub_styles from "../../assets/ProjectCreation/ProjectForm.module.css";
+import VoteProjectList from "./VoteProjectList";
 const VoteForm = () => {
   return (
-    <div>
-      {
-        <h4
-          style={{
-            color: "white",
-            textAlign: "center",
-          }}
-        >
-          투표 페이지로 사용될 예정입니다.
-        </h4>
-      }
+    <div className={styles.project_vote_form}>
+      <div className={styles.title_form}>
+        <div className={styles.title}>마음에 드는 프로젝트에</div>
+        <div className={styles.title}>투표해주세요</div>
+        <p className={styles.title_caption}>최대 3개</p>
+      </div>
 
-      <img
-        src={dogImage}
-        alt="임시 이미지"
-        style={{
-          width: "100%", // 부모 요소의 너비에 맞게 이미지 크기 조정
-          height: "auto", // 높이는 자동으로 비율 유지
-          objectFit: "contain", // 이미지 비율을 유지하며 부모 요소에 맞게 이미지 크기 조정
-        }}
-      ></img>
+      <VoteProjectList />
+
+      <button
+        type="submit"
+        className={sub_styles.submit_button}
+        style={{ marginTop: "20px", marginBottom: "100px", cursor: "pointer" }}
+      >
+        투표 완료
+      </button>
     </div>
   );
 };
