4cabe024bdbac28a39b2f08d1bdcb2fef608fba0
seizethedayunhui <seizethedayunhui@naver.com>
2024-11-26 16:30:33 +0900

    feature: 투표페이지 이동 제한 설정

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
@@ -10,6 +10,7 @@ const Menu = ({
   handleLogout,
 }) => {
   const navigate = useNavigate();
+  const [canNavigate, setCanNavigate] = useState(false);
 
   const handleNavigationWithAuth = (path) => {
     const token = Cookies.get("authToken");
@@ -19,6 +20,31 @@ const Menu = ({
     } else {
       navigate(path);
       toggleMenu();
+    }
+  };
+
+  useEffect(() => {
+    // 특정 시간을 설정 (2024년 11월 29일 오후 6시 30분)
+    const allowedDate = new Date("2024-11-29T18:30:00");
+    const now = new Date();
+
+    if (now >= allowedDate) {
+      setCanNavigate(true); // 조건 만족 시 이동 가능
+    } else {
+      const timeUntilAllowed = allowedDate - now;
+
+      // 특정 시간 이후에 상태를 변경
+      setTimeout(() => {
+        setCanNavigate(true);
+      }, timeUntilAllowed);
+    }
+  }, []);
+
+  const handleVotePageNavigate = () => {
+    if (canNavigate) {
+      handleNavigationWithAuth("/vote"); // 이동할 페이지 경로
+    } else {
+      alert("투표는 2024년 11월 29일 오후 6시 30분부터 가능합니다.");
     }
   };
 
@@ -58,7 +84,7 @@ const Menu = ({
               Create Project
             </li>
             <hr className="line"></hr>
-            <li onClick={() => handleNavigationWithAuth("/vote")}>Vote</li>
+            <li onClick={() => handleVotePageNavigate("/vote")}>Vote</li>
             <hr className="line"></hr>
             <li
               onClick={() => {
