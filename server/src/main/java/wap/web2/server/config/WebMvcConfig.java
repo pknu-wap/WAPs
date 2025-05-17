7a08dcce50668d874fc95b0c7d026c1b12fe386e
gyunho <gyunh01@pukyong.ac.kr>
2025-05-17 16:27:10 +0900

    docs: 환경설정 파일 추가

--- server/src/main/java/wap/web2/server/config/WebMvcConfig.java
+++ server/src/main/java/wap/web2/server/config/WebMvcConfig.java
@@ -10,16 +10,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
-    @Value("${app.cors.allowedOrigins}")
-    private String[] allowedOrigins;
+    //@Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins = {
+            "https://wap-projects.netlify.app",
+            "http://localhost:3000",
+            "http://localhost:8080"
+    };
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins(allowedOrigins)
-        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true)
-        .maxAge(MAX_AGE_SECS);
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
