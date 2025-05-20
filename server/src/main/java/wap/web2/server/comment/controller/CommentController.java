ae270b764d077060fedca5f28bfe054850281251
kangrae <kangrae0501@gmail.com>
2025-05-20 12:53:11 +0900

    fix: pathVariable 추가
    - api 요청을 올바르게 받도록 함

--- server/src/main/java/wap/web2/server/comment/controller/CommentController.java
+++ server/src/main/java/wap/web2/server/comment/controller/CommentController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wap.web2.server.comment.dto.request.CommentCreateRequest;
-import wap.web2.server.comment.dto.request.CommentDeleteRequest;
 import wap.web2.server.comment.service.CommentService;
 import wap.web2.server.ouath2.security.CurrentUser;
 import wap.web2.server.ouath2.security.UserPrincipal;
@@ -23,20 +22,21 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/{projectId}")
-    public ResponseEntity<?> createComment(@PathVariable Long projectId, @RequestBody CommentCreateRequest request,
+    public ResponseEntity<?> createComment(@PathVariable("projectId") Long projectId,
+                                           @RequestBody CommentCreateRequest request,
                                            @CurrentUser UserPrincipal userPrincipal) {
         commentService.save(projectId, request, userPrincipal);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<String> deleteBook(@PathVariable Long commentId, @RequestBody CommentDeleteRequest request,
-                                             @CurrentUser UserPrincipal userPrincipal) {
-        boolean isDeleted = commentService.deleteCommentByPassword(commentId, request, userPrincipal);
-        if (isDeleted) {
+    public ResponseEntity<String> deleteComment(@PathVariable("commentId") Long commentId,
+                                                @CurrentUser UserPrincipal userPrincipal) {
+        try {
+            commentService.delete(commentId, userPrincipal);
             return ResponseEntity.ok("댓글이 삭제되었습니다.");
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("댓글 비밀번호가 일치하지 않습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
         }
     }
 }
