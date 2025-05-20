f823f839c3596d7f5ca1729907b390b9b47c4306
gyunho <gyunh01@pukyong.ac.kr>
2025-05-20 21:06:35 +0900

    refactor: 버킷에 저장되는 패키지 구조에 연도와 학기 추가
    - S3 버킷에는 다음의 형태로 이미지가 저장됨
    {디렉토리 이름}/{연도-학기}/{프로젝트 명}/{이미지 타입}/{업로드 시간}_{원본 파일명}

--- server/src/main/java/wap/web2/server/aws/AwsUtils.java
+++ server/src/main/java/wap/web2/server/aws/AwsUtils.java
@@ -1,5 +1,6 @@
 package wap.web2.server.aws;
 
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,6 +13,7 @@ import wap.web2.server.config.AwsS3Config;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import wap.web2.server.project.dto.request.ProjectCreateRequest;
 
 @Service
 @RequiredArgsConstructor
@@ -24,25 +26,23 @@ public class AwsUtils {
     private final S3Client s3Client;
     private final AwsS3Config s3Config;
 
-    /**
-     * projects/{project-title}/thumbnail/ 와 projects/{project-title}/images/ 에 항목에 저장됩니다.
-     * @param dirName: 저장할 최상위 디렉토리 이름
-     * @param projectName: 디렉토리명이 될 프로젝트 이름
-     * @param imageFiles: 프로젝트의 이미지
-     * @return 이미지가 저장된 주소
-     */
-    public List<String> uploadImagesTo(String dirName, String projectName, String imageType, List<MultipartFile> imageFiles) throws IOException {
+    public List<String> uploadImagesTo(String dirName, Integer projectYear, Integer semester, String projectName,String imageType, List<MultipartFile> imageFiles)
+            throws IOException {
         List<String> imageUrls = new ArrayList<>();
+
         for (MultipartFile imageFile : imageFiles) {
-            String imageUrl = uploadImageTo(dirName, projectName, imageType, imageFile);
+            String imageUrl = uploadImageTo(dirName, projectYear, semester, projectName, imageType, imageFile);
             imageUrls.add(imageUrl);
         }
         return imageUrls;
     }
 
-    public String uploadImageTo(String dirName, String projectName, String imageType, MultipartFile imageFile) throws IOException {
+    public String uploadImageTo(String dirName, Integer projectYear, Integer semester, String projectName,
+                                String imageType, MultipartFile imageFile)
+            throws IOException {
         String originalFileName = getOriginalFileName(imageFile);
-        String fileName = createTimestampFileName(dirName, projectName, imageType, originalFileName);
+        String fileName = createTimestampFileName(dirName, projectYear, semester, projectName, imageType,
+                originalFileName);
 
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(s3Config.getBucketName())
@@ -69,13 +69,14 @@ public class AwsUtils {
         return originalFilename;
     }
 
-    //TODO: 학기 연도 추가하기
     /**
-     * 파일 이름 중복 방지를 위해 랜덤 파일명 생성 원본 파일명 앞에 현재 시간을 붙인다.
-     * 이미지 타입은 썸네일인지 본문 이미지인지 구분한다.
-     * @return: {디렉토리 이름}/{프로젝트 명}/{이미지 타입}/{업로드 시간}_{원본 파일명}
+     * 파일 이름 중복 방지를 위해 랜덤 파일명 생성 원본 파일명 앞에 현재 시간을 붙인다. 이미지 타입은 썸네일인지 본문 이미지인지 구분한다.
+     *
+     * @return: {디렉토리 이름}/{연도-학기}/{프로젝트 명}/{thumbnail or images}/{업로드 시간}_{원본 파일명}
      */
-    private String createTimestampFileName(String dirName, String projectName, String imageType, String originName) {
-        return dirName + "/" + projectName + "/" + imageType + "/" + System.currentTimeMillis() + "_" + originName;
+    private String createTimestampFileName(String dirName, Integer year, Integer semester, String projectName,
+                                           String imageType, String originName) {
+        return dirName + "/" + year + "-" + semester + "/" + projectName + "/" + imageType + "/" +
+                System.currentTimeMillis() + "_" + originName;
     }
 }
