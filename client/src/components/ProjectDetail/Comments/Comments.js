f970362ef8e51d0fb1b54848dcd8e6415affce7b
yuni <seizethedayunhui@naver.com>
2025-05-21 13:41:14 +0900

    modify : 로그인 없이 댓글 작성 시  alert창 메시지 수정 및 navigate 경로 설정

--- client/src/components/ProjectDetail/Comments/Comments.js
+++ client/src/components/ProjectDetail/Comments/Comments.js
@@ -82,7 +82,8 @@ const Comments = ({ projectId }) => {
       window.location.reload();
     } catch (error) {
       // console.error("댓글 작성 실패:", error);
-      alert("댓글 작성에 실패했습니다. 다시 시도해주세요.");
+      alert("댓글은 로그인 후에 작성 가능합니다. ");
+      navigate("/login");
     }
   };
 
