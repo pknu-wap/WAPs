38c2d7d0b6142fa3785edf0da7fd341dfe36b704
gyunho <gyunh01@pukyong.ac.kr>
2025-05-31 14:43:19 +0900

    refactor: 이미지 삭제 쿼리 직접 작성

--- server/src/main/java/wap/web2/server/project/repository/ImageRepository.java
+++ server/src/main/java/wap/web2/server/project/repository/ImageRepository.java
@@ -1,8 +1,14 @@
 package wap.web2.server.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import wap.web2.server.project.entity.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
-    void deleteByImageFile(String imageFile);
+
+    @Modifying
+    @Query("DELETE FROM Image i WHERE i.imageFile = :imageFile")
+    void deleteByImageFile(@Param("imageFile") String imageFile);
 }
