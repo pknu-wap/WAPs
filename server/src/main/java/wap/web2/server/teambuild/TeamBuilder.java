6ae075fd6a0b1f59701e29918035bb756c8c5a24
gyunho <gyunh01@pukyong.ac.kr>
2025-08-29 18:15:46 +0900

    chore: 코드 이해를 위한 설명 주석 추가

--- server/src/main/java/wap/web2/server/teambuild/TeamBuilder.java
+++ server/src/main/java/wap/web2/server/teambuild/TeamBuilder.java
@@ -14,6 +14,7 @@ public class TeamBuilder {
                                               Map<String, LeaderPriority> leaderPriorityMap) {
         Map<String, List<String>> teams = new HashMap<>();
 
+        // 팀장이 원하는대로 팀을 구성
         for (Map.Entry<String, LeaderPriority> entry : leaderPriorityMap.entrySet()) {
             String teamName = entry.getKey();
             teams.put(teamName, new LinkedList<>());
@@ -21,6 +22,7 @@ public class TeamBuilder {
             List<String> initialTeam = new LinkedList<>();
 
             for (int i = 0; i < num; ++i) {
+                // 지원자를 queue에서 제거해 임시 구성팀에게 할당한다.
                 String first = entry.getValue().getMemberNames().poll();
                 initialTeam.add(first);
             }
@@ -28,13 +30,18 @@ public class TeamBuilder {
             teams.get(teamName).addAll(initialTeam);
         }
 
+        // 일단 구성된 팀에서 중복된 인원을 제거하며 팀원을 할당합니다.
         while (true) {
+            // 먼저 여러 팀에 합류하고 있는 인원을 골라냅니다.
             Set<String> names = this.getDuplicateMemberOf(teams);
+
+            // 중복되는 인원이 없다면 모든 팀이 완성된 것입니다.
             if (names.isEmpty()) {
                 return teams;
             }
 
             for (String name : names) {
+                // 중복되는 인원은 자신이 가장 원하는 팀에 우선적으로 합류됩니다.
                 this.traceToMaxPriority(name, teams, applyMap, leaderPriorityMap);
             }
         }
@@ -57,19 +64,31 @@ public class TeamBuilder {
         return duplicateNames;
     }
 
+    /**
+     * 여러 팀에 합류해있는 사람은 우선순위가 가장 높은 팀으로 합류된다. 팀원을 잃은 팀은 새로운 멤버를 추가한다. 이 멤버는 leaderPriorityMap에 존재한다.
+     * @param name: 여러 팀에 합류해있는 사람
+     * @param teams: 현재까지 완성된 팀 명단
+     * @param applyMap: 모든 동아리원의 지원서를 담는 Map
+     * @param leaderPriorityMap: 팀장이 희망하는 팀원 명단
+     */
     private void traceToMaxPriority(String name, Map<String, List<String>> teams, Map<String, List<Apply>> applyMap,
                                     Map<String, LeaderPriority> leaderPriorityMap) {
-        List<Apply> applies = applyMap.get(name);
+        List<Apply> applies = applyMap.get(name); // 전달받은
         boolean isJoin = false;
 
         for (Apply apply : applies) {
             String teamName = apply.getProjectName();
             List<String> members = teams.get(teamName);
+
             if (members.contains(name)) {
+                // 가장 높은 우선순위를 가진 팀에게 할당
                 if (!isJoin) {
+                    // 할당 - (아무런 조치도 취하지 않음)
                     isJoin = true;
                 } else {
+                    // 지원자를 팀에서 빼야 함.
                     members.remove(name);
+                    // 새로운 지원자를 팀으로 합류시킴
                     this.addNewMember(teamName, members, leaderPriorityMap);
                 }
             }
@@ -77,9 +96,11 @@ public class TeamBuilder {
 
     }
 
+    // 현재 남아있는 팀원 명단 중 팀장이 가장 원하는 인원을 추가합니다.
     private void addNewMember(String teamName, List<String> members, Map<String, LeaderPriority> leaderPriorityMap) {
         Queue<String> memberNames = leaderPriorityMap.get(teamName).getMemberNames();
         if (!memberNames.isEmpty()) {
+            // 큐의 front에는 팀장이 가장 원하는 팀원이 있음
             String newMember = memberNames.poll();
             members.add(newMember);
         }
