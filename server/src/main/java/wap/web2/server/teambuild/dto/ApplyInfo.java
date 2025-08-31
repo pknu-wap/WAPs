5a6a9ea766a1145ca8e053ed68adcb10d2c02342
kangrae <kangrae0501@gmail.com>
2025-08-31 17:11:59 +0900

    feat: apply 정보를 map으로 변환하는 로직 구현

--- server/src/main/java/wap/web2/server/teambuild/dto/ApplyInfo.java
+++ server/src/main/java/wap/web2/server/teambuild/dto/ApplyInfo.java
@@ -1,12 +1,20 @@
 package wap.web2.server.teambuild.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import wap.web2.server.teambuild.entity.Position;
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApplyInfo {
 
-    private Integer priority;
-    private Position position;
     private Long userId;
     private Long projectId;
+    private Integer priority;
+    private Position position;
 
 }
--- server/src/main/java/wap/web2/server/teambuild/service/TeamBuildService.java
+++ server/src/main/java/wap/web2/server/teambuild/service/TeamBuildService.java
@@ -1,10 +1,17 @@
 package wap.web2.server.teambuild.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import wap.web2.server.member.repository.UserRepository;
 import wap.web2.server.ouath2.security.UserPrincipal;
 import wap.web2.server.project.repository.ProjectRepository;
+import wap.web2.server.teambuild.dto.ApplyInfo;
+import wap.web2.server.teambuild.dto.RecruitInfo;
+import wap.web2.server.teambuild.entity.ProjectApply;
 import wap.web2.server.teambuild.repository.ProjectApplyRepository;
 import wap.web2.server.teambuild.repository.ProjectRecruitRepository;
 import wap.web2.server.teambuild.repository.ProjectRecruitWishRepository;
@@ -22,9 +29,35 @@ public class TeamBuildService {
 
     // TODO: 리턴값 고민
     public void makeTeam(UserPrincipal userPrincipal) {
-        // TODO: 레포지토리 뒤져서 map화 하고, teamBuilder의 인자로 주입할 예정
+        Map<Long, List<ApplyInfo>> applyMap = getApplyMap();
+        Map<Long, RecruitInfo> RecruitMap = getRecruitMap();
 
         TeamBuilder teamBuilder = new TeamBuilderImpl();
+
+        return;
+    }
+
+    private Map<Long, List<ApplyInfo>> getApplyMap() {
+        Map<Long, List<ApplyInfo>> applyMap = new HashMap<>();
+
+        List<ProjectApply> applyEntities = applyRepository.findAll();
+        for (ProjectApply applyEntity : applyEntities) {
+            long projectId = applyEntity.getProject().getProjectId();
+            applyMap.computeIfAbsent(projectId, key -> new ArrayList<>())
+                    .add(ApplyInfo.builder()
+                            .userId(applyEntity.getUser().getId())
+                            .projectId(projectId)
+                            .priority(applyEntity.getPriority())
+                            .position(applyEntity.getPosition())
+                            .build()
+                    );
+        }
+
+        return applyMap;
+    }
+
+    private Map<Long, RecruitInfo> getRecruitMap() {
+
     }
 
 }
