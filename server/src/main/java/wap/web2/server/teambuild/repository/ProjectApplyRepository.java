cfa143ed9c9a895967980b42c235eeaf422a5aa3
kangrae <kangrae0501@gmail.com>
2025-09-04 22:15:37 +0900

    fix: repository 파라미터 명시

--- server/src/main/java/wap/web2/server/teambuild/repository/ProjectApplyRepository.java
+++ server/src/main/java/wap/web2/server/teambuild/repository/ProjectApplyRepository.java
@@ -3,6 +3,7 @@ package wap.web2.server.teambuild.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import wap.web2.server.project.entity.Project;
 import wap.web2.server.teambuild.dto.TeamMemberResult;
@@ -17,5 +18,5 @@ public interface ProjectApplyRepository extends JpaRepository<ProjectApply, Long
     List<ProjectApply> findAllBySemesterAndPosition(String semester, Position position);
 
     @Query("select DISTINCT new wap.web2.server.teambuild.dto.TeamMemberResult(p.user.id, p.user.name, p.position) from ProjectApply p where p.user.id in :userIds")
-    List<TeamMemberResult> findAllByUserId(List<Long> userIds);
+    List<TeamMemberResult> findAllByUserId(@Param("userIds") List<Long> userIds);
 }
