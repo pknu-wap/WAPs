c40e5c82c955fd97cfd5a4356f6c4a325cd1352e
kangrae <kangrae0501@gmail.com>
2025-10-13 10:54:50 +0900

    refactor: 기존 팀 빌딩 api 비활성화

---
+++ server/src/main/java/wap/web2/server/teambuild/controller/TeamBuildControllerV3.java
@@ -0,0 +1,13 @@
+package wap.web2.server.teambuild.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/team-build")
+@RequiredArgsConstructor
+public class TeamBuildControllerV3 {
+
+
+}
