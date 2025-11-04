86a1d4f0092d8a5ef266c68dbf0cf8af6c88a6dc
kangrae <kangrae0501@gmail.com>
2025-11-04 15:42:36 +0900

    feat: Event List를 반환하는 컨트롤러 구현
    - 레코드를 사용해서 Dto 생성
    - 서비스에서 던져지는 명시적인 오류는 없지만 컨트롤러단에서 일단 트라이 캐치함

---
+++ server/src/main/java/wap/web2/server/calendar/controller/CalendarController.java
@@ -0,0 +1,34 @@
+package wap.web2.server.calendar.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wap.web2.server.calendar.dto.EventsResponse;
+import wap.web2.server.calendar.service.CalendarService;
+
+@RestController
+@RequestMapping("/calendar")
+@RequiredArgsConstructor
+public class CalendarController {
+
+    private final CalendarService calendarService;
+
+    @GetMapping()
+    public ResponseEntity<?> getActiveEvents() {
+        try {
+            List<EventsResponse> events = calendarService.getActiveEvents()
+                    .stream()
+                    .map(EventsResponse::from)
+                    .toList();
+
+            return ResponseEntity.ok(events);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+}
