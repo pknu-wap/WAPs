1e487527fb39bde536ea1450957ec5642f677b2d
이승훈 <shlee4167@naver.com>
2025-11-09 22:13:49 +0900

    Revert "chore: 어드민 페이지 레이아웃 분리 (#415)"
    This reverts commit c7aca1a83cd92c3ba92360391dbec0449a306dfc.

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import VoteResultPage from "./pages/VoteResultPage";
 import RoleSelectPage from "./pages/RoleSelectPage";
 import TeamBuildPage from "./pages/TeamBuildPage";
 import TeamBuildResultPage from "./pages/TeamBuildResultPage";
-import UserPermissionPage from "./pages/adminPages/UserPermissionPage";
+
 import "./App.css";
 
 function App() {
@@ -24,13 +24,10 @@ function App() {
         <Routes>
           {/* 기본 홈 화면 */}
           <Route path="/" element={<SplashPage />} />
-
           {/* 로그인 화면 */}
           <Route path="/login" element={<Login />} />
-
           {/* 카카오 인증 Callback */}
           <Route path="/oauth/callback" element={<Callback />} />
-
           {/* 경로 */}
           <Route path="/project/create" element={<ProjectCreatePage />} />
           <Route path="/vote" element={<VotePage />} />
@@ -41,21 +38,10 @@ function App() {
           <Route path="/select/role" element={<RoleSelectPage />} />
           <Route path="/team-build" element={<TeamBuildPage />} />
           <Route path="/team-build/result" element={<TeamBuildResultPage />} />
-
           {/* 보호된 페이지 */}
           <Route path="/protected" element={<ProtectedPage />} />
         </Routes>
       </div>
-
-      {/* 관리자 페이지*/}
-      <div className="adminContainer">
-        <Routes>
-          <Route path="/admin">
-            <Route path="user-permission" element={<UserPermissionPage />} />
-          </Route>
-        </Routes>
-      </div>
-
     </Router>
   );
 }
