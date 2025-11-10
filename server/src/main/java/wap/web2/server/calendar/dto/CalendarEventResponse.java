2c0c14e12612ef75d0d3687d8eebb8558b0f5a48
kangrae <kangrae0501@gmail.com>
2025-11-10 22:31:10 +0900

    fix(CalendarEventResponse): LocalDateTime Json 포맷 지정
    - yyyy-MM-dd HH:mm으로 지정하여 프론트에서 정제된 String으로 받을 수 있도록 함

--- server/src/main/java/wap/web2/server/calendar/dto/CalendarEventResponse.java
+++ server/src/main/java/wap/web2/server/calendar/dto/CalendarEventResponse.java
@@ -1,20 +1,22 @@
 package wap.web2.server.calendar.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import wap.web2.server.calendar.entity.CalendarEvent;
 
 public record CalendarEventResponse(
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+        LocalDateTime date,
         String title,
         String content,
-        LocalDateTime date,
         String target
 ) {
 
     public static CalendarEventResponse from(CalendarEvent event) {
         return new CalendarEventResponse(
+                event.getDate(),
                 event.getTitle(),
                 event.getContent(),
-                event.getDate(),
                 event.getTarget()
         );
     }
