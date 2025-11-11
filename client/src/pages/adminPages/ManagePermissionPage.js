482117c275e5b00cf1f833860952483be84cf811
SH-MooDy <shlee4167@naver.com>
2025-11-12 00:35:22 +0900

    feat: 권한관리페이지 기초  레이아웃 구성

--- client/src/pages/adminPages/ManagePermissionPage.js
+++ client/src/pages/adminPages/ManagePermissionPage.js
@@ -1,7 +1,26 @@
+import styles from "../../assets/Admin/ManagePermission.module.css";
+
 const ManagePermissionPage = () => {
     return (
-        <div>
-            유저 권한 관리
+        <div className={styles.container}>
+            <div className={styles.Box}>
+                <div className={styles.header}>
+                    <span>목록</span>
+                    <button className={styles.onlyBtn}>ONLY MEMBER</button>
+                </div>
+                <div className={styles.memberList}></div>
+            </div>
+            <div className={styles.Box}>
+                <span className={styles.header}>권한 변경</span>
+                <div className={styles.changeContent}>
+                    <div className={styles.controler}>
+                        <span style={{ fontSize: "20px", fontWeight: "700" }}>다음으로 권한 변경:</span>
+                        <button className={styles.role}></button>
+                        <button className={styles.summit}>적용</button>
+                    </div>
+                    <div className={styles.changeList}></div>
+                </div>
+            </div>
         </div>
     );
 }
