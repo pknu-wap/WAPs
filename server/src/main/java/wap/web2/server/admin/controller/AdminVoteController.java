dd59655ab81ecab007c2f6e10220bfff09e13dca
gyunho <gyunh01@pukyong.ac.kr>
2025-11-13 22:50:22 +0900

    chore: 투표 요청 dto의 컬럼에 null을 허용하지 않도록 변경

--- server/src/main/java/wap/web2/server/admin/controller/AdminVoteController.java
+++ server/src/main/java/wap/web2/server/admin/controller/AdminVoteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import wap.web2.server.admin.service.AdminVoteService;
 import wap.web2.server.security.core.CurrentUser;
 import wap.web2.server.security.core.UserPrincipal;
-import wap.web2.server.util.SemesterFormat;
+import wap.web2.server.util.Semester;
 
 @RestController
 @RequestMapping("/admin/vote")
@@ -23,7 +23,7 @@ public class AdminVoteController {
     @Operation(summary = "투표 열기",
             description = "희망하는 학기의 투표를 생성합니다. 혹은 희망하는 학기의 투표를 엽니다. 이제 투표를 진행할 수 있습니다.")
     public ResponseEntity<?> openVoteMeta(@CurrentUser UserPrincipal currentUser,
-                                          @RequestParam("semester") @SemesterFormat String semester) {
+                                          @RequestParam("semester") @Semester String semester) {
         adminVoteService.initializeVote(semester, currentUser.getId());
         return ResponseEntity.ok().build();
     }
@@ -31,7 +31,7 @@ public class AdminVoteController {
     @PostMapping("/closed")
     @Operation(summary = "투표 닫기", description = "희망하는 학기의 투표를 닫습니다. 그럼 더이상 투표를 진행할 수 없습니다.")
     public ResponseEntity<?> closeVoteMeta(@CurrentUser UserPrincipal currentUser,
-                                           @RequestParam("semester") @SemesterFormat String semester) {
+                                           @RequestParam("semester") @Semester String semester) {
         adminVoteService.closeVote(semester, currentUser.getId());
         return ResponseEntity.ok().build();
     }
--- server/src/main/java/wap/web2/server/vote/dto/VoteRequest2.java
+++ server/src/main/java/wap/web2/server/vote/dto/VoteRequest2.java
@@ -3,14 +3,15 @@ package wap.web2.server.vote.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
-import wap.web2.server.util.SemesterFormat;
+import wap.web2.server.util.Semester;
 
 public record VoteRequest2(
+        @NotNull
         @Size(min = 3, max = 3, message = "3개의 프로젝트에 투표해야합니다.")
         List<Long> projectIds,
 
         @NotNull
-        @SemesterFormat
+        @Semester
         String semester
 ) {
 }
