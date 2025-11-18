661387e0c1968d393dd2dee5506bd21330f0e0eb
SH-MooDy <shlee4167@naver.com>
2025-11-18 13:27:35 +0900

    Merge branch 'develop' of https://github.com/pknu-wap/WAPs into feat/#440-vote

--- client/src/pages/adminPages/ManageVotePage.js
+++ client/src/pages/adminPages/ManageVotePage.js
@@ -3,14 +3,14 @@ import styles from "../../assets/Admin/ManageVote.module.css";
 import apiClient from "../../utils/api";
 
 const ManageVotePage = () => {
-    const [votingStatus, setVotingStatus] = useState("BEFORE"); // 투표 상태 정보 (BEFORE, IN_PROGRESS, AFTER)
+    const [votingStatus, setVotingStatus] = useState(null); // 투표 상태 정보 (BEFORE, IN_PROGRESS, AFTER)
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchVoteStart = async () => {
             try {
-                const response = await apiClient.post("/admin/vote/open");
+                const response = await apiClient.post("/vote/now");
                 setVotingStatus("IN_PROGRESS");
             } catch {
 
