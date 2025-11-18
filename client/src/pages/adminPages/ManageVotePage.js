170460d62bccecf1dd1368502dc22fe3257c9d91
SH-MooDy <shlee4167@naver.com>
2025-11-18 23:18:54 +0900

    feat:연도 구현

--- client/src/pages/adminPages/ManageVotePage.js
+++ client/src/pages/adminPages/ManageVotePage.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import styles from "../../assets/Admin/ManageVote.module.css";
 import apiClient from "../../utils/api";
 import { getCurrentSemester } from "../../utils/dateUtils";
@@ -7,28 +8,38 @@ const ManageVotePage = () => {
     const [voteStatus, setVoteStatus] = useState(null); // 투표 상태 (PRE_VOTE, VOTING, ENDED)
     const [semester, setSemester] = useState(null); // 현재 학기
     const [isProcessing, setIsProcessing] = useState(false); // 열기,닫기 버튼 누를 때 로딩 상태
+
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+
+    const [searchParams, setSearchParams] = useSearchParams(); // 쿼리 파라미터를 위한 상태
+    const [projects, setProjects] = useState([]); // 프로젝트 목록 상태
+    const [selectedProjects, setSelectedProjects] = useState([]); // 선택된 프로젝트 ID 목록
 
     useEffect(() => {
         setSemester(getCurrentSemester());
     }, []);
 
+    // 투표 상태 조회
+    // useEffect(() => {
+    //     const fetchVoteStart = async () => {
+    //         try {
+    //             const response = await apiClient.post("/vote/now");
+    //             setVoteStatus(response.data.isOpen);
+
+    //         } catch (e) {
+    //             setError("투표 상태 조회 실패");
+    //         } finally {
+    //             setIsLoading(false);
+    //         }
+    //     };
+    //     fetchVoteStart();
+    // }, []);
+
     useEffect(() => {
-        const fetchVoteStart = async () => {
-            try {
-                const response = await apiClient.post("/vote/now");
-                setVoteStatus(response.data.isOpen);
+        if (!semester) return;
 
-            } catch (e) {
-                setError("투표 상태 조회 실패");
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        fetchVoteStart();
-    }, []);
-
+    })
     // 투표 열기 핸들러
     const handleOpenVote = async () => {
         if (voteStatus !== "PRE_VOTE") return;
@@ -61,22 +72,47 @@ const ManageVotePage = () => {
     }
 
 
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error}</div>;
+    //if (isLoading) return <div>Loading...</div>;
+    //if (error) return <div>{error}</div>;
     return (
         <div className={styles.container}>
+
+            <div className={styles.upperBox}>
+                <div className={styles.voteSemester}>{semester}</div>
+                <button
+                    disabled={isProcessing}
+                    onClick={handleOpenVote}
+                    className={styles.openBtn}
+                >
+                    {isProcessing ? "Opening.." : "OPEN"}
+                </button>
+            </div>
+            <div className={styles.bar}></div>
+            <div className={styles.cardGrid}>
+
+            </div>
+            <button className={styles.summit}>제출</button>
+
+
+
+
+
+
             {/* PRE_VOTE 상태 */}
             {voteStatus === "PRE_VOTE" && (
-                <div className={styles.PreVoteBox}>
-                    <div className={styles.voteSemester}>{semester}</div>
-                    <button
-                        disabled={isProcessing}
-                        onClick={handleOpenVote}
-                        className={styles.openBtn}
-                    >
-                        {isProcessing ? "Opening.." : "OPEN"}
-                    </button>
+                <div>
+                    <div className={styles.upperBox}>
+                        <div className={styles.voteSemester}>{semester}</div>
+                        <button
+                            disabled={isProcessing}
+                            onClick={handleOpenVote}
+                            className={styles.openBtn}
+                        >
+                            {isProcessing ? "Opening.." : "OPEN"}
+                        </button>
+                    </div>
                 </div>
+
             )}
 
             {/* VOTING 상태 */}
