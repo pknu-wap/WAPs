dfee494484deffb6ca7d729e623bb3050cee5e10
SH-MooDy <shlee4167@naver.com>
2025-11-18 14:57:25 +0900

    feat: 투표 관리 기초 상태 설정 및 투표 전 레이아웃 구현

--- client/src/pages/adminPages/ManageVotePage.js
+++ client/src/pages/adminPages/ManageVotePage.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import styles from "../../assets/Admin/ManageVote.module.css";
 import apiClient from "../../utils/api";
+import { getCurrentSemester } from "../../utils/dateUtils";
 
 const ManageVotePage = () => {
-    const [votingStatus, setVotingStatus] = useState(null); // 투표 상태 정보 (BEFORE, IN_PROGRESS, AFTER)
+    const [VoteStatus, setVoteStatus] = useState(null); // 투표 상태
+    const [semester, setSemester] = useState(null); // 현재 학기
+    const [isProcessing, setIsProcessing] = useState(false); // 열기,닫기 버튼 누를 때 로딩 상태
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -11,9 +14,25 @@ const ManageVotePage = () => {
         const fetchVoteStart = async () => {
             try {
                 const response = await apiClient.post("/vote/now");
-                setVotingStatus("IN_PROGRESS");
-            } catch {
 
+                // 투표 전
+                if ((response.data.isOpen) === true) {
+                    setVoteStatus("BEFORE");
+
+                    return (
+                        <div className={styles.beforeVoteBox}>
+                            <button className={styles.voteSemester}>{getCurrentSemester()}</button>
+                            <div className={styles.openBtn}>OPEN</div>
+                        </div>
+                    );
+                } else {
+
+                }
+
+            } catch (e) {
+                setError("투표 상태 조회 실패");
+            } finally {
+                setIsLoading(false);
             }
         };
         fetchVoteStart();
