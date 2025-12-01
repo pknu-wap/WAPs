9543c9a8d76020b10623ceecfe048b75748312f8
gyunho <gyunh01@pukyong.ac.kr>
2025-12-01 21:20:47 +0900

    feat: Refresh Token을 통해 새로운 Access Token을 발급
    - 기존 Refresh Token를 대체하여 새로운 Refresh Token이 저장된다.
    - 만약 Refresh Token의 만료기간이 지났다면 삭제한다.

--- server/src/main/java/wap/web2/server/auth/controller/AuthController.java
+++ server/src/main/java/wap/web2/server/auth/controller/AuthController.java
@@ -1,7 +1,11 @@
 package wap.web2.server.auth.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import java.net.URI;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,10 +24,15 @@ import wap.web2.server.auth.payload.LoginRequest;
 import wap.web2.server.auth.payload.SignUpRequest;
 import wap.web2.server.exception.BadRequestException;
 import wap.web2.server.member.entity.AuthProvider;
+import wap.web2.server.member.entity.RefreshToken;
 import wap.web2.server.member.entity.Role;
 import wap.web2.server.member.entity.User;
+import wap.web2.server.member.repository.RefreshTokenRepository;
 import wap.web2.server.member.repository.UserRepository;
+import wap.web2.server.security.config.AppProperties;
+import wap.web2.server.security.core.UserPrincipal;
 import wap.web2.server.security.jwt.TokenProvider;
+import wap.web2.server.util.CookieUtils;
 
 @RestController
 @RequestMapping("/auth")
@@ -34,15 +43,15 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final AppProperties appProperties;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.getEmail(),
-                        loginRequest.getPassword()
-                )
-        );
+                        loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
@@ -74,6 +83,59 @@ public class AuthController {
 
         return ResponseEntity.created(location)
                 .body(new ApiResponse(true, "User registered successfully@"));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = CookieUtils.getCookie(request, "refresh_token")
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new BadRequestException("Refresh Token not found"));
+
+        if (!tokenProvider.validateToken(refreshToken)) {
+            throw new BadRequestException("Invalid Refresh Token");
+        }
+
+        RefreshToken token = refreshTokenRepository.findByToken(refreshToken)
+                .orElseThrow(() -> new BadRequestException("Refresh Token not found in database"));
+
+        validateToken(token);
+        Authentication authentication = createAuthentication(token);
+
+        String newAccessToken = tokenProvider.createToken(authentication);
+        String newRefreshToken = saveNewRefreshToken(token, authentication);
+
+        addRefreshTokenCookieTo(response, newRefreshToken);
+        return ResponseEntity.ok(new AuthResponse(newAccessToken));
+    }
+
+    private void addRefreshTokenCookieTo(HttpServletResponse response, String newRefreshToken) {
+        long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpirationMsec();
+        int cookieMaxAge = (int) refreshTokenExpiry / 1000;
+
+        CookieUtils.addCookie(response, "refresh_token", newRefreshToken, cookieMaxAge);
+    }
+
+    private String saveNewRefreshToken(RefreshToken token, Authentication authentication) {
+        long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpirationMsec();
+        String newRefreshToken = tokenProvider.createRefreshToken(authentication);
+
+        token.update(newRefreshToken, refreshTokenExpiry);
+        refreshTokenRepository.save(token);
+
+        return newRefreshToken;
+    }
+
+    private Authentication createAuthentication(RefreshToken token) {
+        User user = token.getUser();
+        UserPrincipal userPrincipal = UserPrincipal.create(user);
+        return new UsernamePasswordAuthenticationToken(userPrincipal, null, userPrincipal.getAuthorities());
+    }
+
+    private void validateToken(RefreshToken token) {
+        if (token.getExpiryDate().isBefore(Instant.now())) {
+            refreshTokenRepository.delete(token);
+            throw new BadRequestException("Refresh Token expired");
+        }
     }
 
 }
