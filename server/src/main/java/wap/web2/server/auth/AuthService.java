3319c2b1916b397a4838f75ecc579e9e892fed2f
gyunho <gyunh01@pukyong.ac.kr>
2025-12-02 23:55:21 +0900

    refactor: Refresh Token 저장 및 검증 로직을 Service Layer로 분리

---
+++ server/src/main/java/wap/web2/server/auth/AuthService.java
@@ -0,0 +1,62 @@
+package wap.web2.server.auth;
+
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wap.web2.server.exception.BadRequestException;
+import wap.web2.server.member.entity.RefreshToken;
+import wap.web2.server.member.entity.User;
+import wap.web2.server.member.repository.RefreshTokenRepository;
+import wap.web2.server.security.config.AppProperties;
+import wap.web2.server.security.core.UserPrincipal;
+import wap.web2.server.security.jwt.TokenProvider;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final AppProperties appProperties;
+
+    @Transactional
+    public Tokens createNewToken(String token) {
+        if (!tokenProvider.validateToken(token)) {
+            throw new BadRequestException("Invalid Refresh Token");
+        }
+
+        RefreshToken refreshToken = refreshTokenRepository.findByToken(token)
+                .orElseThrow(() -> new BadRequestException("Refresh Token not found"));
+
+        validateToken(refreshToken);
+        Authentication authentication = createAuthentication(refreshToken);
+
+        String newAccessToken = tokenProvider.createToken(authentication);
+        String newRefreshToken = tokenProvider.createRefreshToken(authentication);
+
+        saveNewRefreshToken(refreshToken, newRefreshToken, authentication);
+        return new Tokens(newAccessToken, newRefreshToken);
+    }
+
+    private void saveNewRefreshToken(RefreshToken token, String newRefreshToken, Authentication authentication) {
+        long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpirationMsec();
+        token.update(newRefreshToken, refreshTokenExpiry);
+        refreshTokenRepository.save(token);
+    }
+
+    private Authentication createAuthentication(RefreshToken token) {
+        User user = token.getUser();
+        UserPrincipal userPrincipal = UserPrincipal.create(user);
+        return new UsernamePasswordAuthenticationToken(userPrincipal, null, userPrincipal.getAuthorities());
+    }
+
+    private void validateToken(RefreshToken token) {
+        if (token.getExpiryDate().isBefore(Instant.now())) {
+            refreshTokenRepository.delete(token);
+            throw new BadRequestException("Refresh Token expired");
+        }
+    }
+}
---
+++ server/src/main/java/wap/web2/server/auth/Tokens.java
@@ -0,0 +1,4 @@
+package wap.web2.server.auth;
+
+public record Tokens(String accessToken, String refreshToken) {
+}
