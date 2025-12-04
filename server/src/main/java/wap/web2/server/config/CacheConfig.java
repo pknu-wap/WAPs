0abf587db248efb18b319e784db73eb39e99d7b7
kangrae <kangrae0501@gmail.com>
2025-12-04 17:21:17 +0900

    chore: 캐시 정책 변경

--- server/src/main/java/wap/web2/server/config/CacheConfig.java
+++ server/src/main/java/wap/web2/server/config/CacheConfig.java
@@ -14,26 +14,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CacheConfig {
 
-    /*
-        사용될 api마다 개별 TTL을 설정한다.
-        현재는 기본적으로 설정을 같게하여 서비스에 끼치는 영향을 파악하기위해 동일한 설정으로 한다.
-     */
+
+    // 사용될 api마다 개별 정책(TTL, Cap)을 설정한다.
     @Bean
     public CacheManager cacheManager() {
         SimpleCacheManager manager = new SimpleCacheManager();
         manager.setCaches(List.of(
                 new CaffeineCache("projectList",
                         Caffeine.newBuilder()
-                                .expireAfterWrite(60, TimeUnit.SECONDS)
-                                .initialCapacity(100)
-                                .maximumSize(1000)
+                                .expireAfterWrite(30, TimeUnit.MINUTES)
+                                .initialCapacity(4)
+                                .maximumSize(20)
                                 .build()
                 ),
                 new CaffeineCache("voteResults",
                         Caffeine.newBuilder()
-                                .expireAfterWrite(60, TimeUnit.SECONDS)
-                                .initialCapacity(100)
-                                .maximumSize(1000)
+                                .expireAfterWrite(60, TimeUnit.MINUTES)
+                                .initialCapacity(4)
+                                .maximumSize(20)
                                 .build()
                 )
         ));
