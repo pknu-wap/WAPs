6810d8e9fa8235256d943510e83c47179dac7e7c
SH-MooDy <shlee4167@naver.com>
2026-01-20 18:34:07 +0900

    refactor: 권한 선택 페이지 api 호출 리팩토링

---
+++ client/src/api/user.js
@@ -0,0 +1,11 @@
+import apiClient from "./client";
+
+// 사용자 관련 API
+export const userApi = {
+    // 회원 역할 선택
+    selectRole: (role) => {
+        const url = role === "member" ? "/user/role/member" : "/user/role/guest";
+        return apiClient.post(url, {});
+    },
+
+}
