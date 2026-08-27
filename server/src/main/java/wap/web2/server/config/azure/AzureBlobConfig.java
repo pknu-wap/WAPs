466429786590a9f2fbbfe002be112e8a0ef5d033
psvm203 <psvm203@proton.me>
2026-08-27 16:51:14 +0900

    fix(ci): AzureBlobConfig에 azure 프로파일을 명시

--- server/src/main/java/wap/web2/server/config/azure/AzureBlobConfig.java
+++ server/src/main/java/wap/web2/server/config/azure/AzureBlobConfig.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("azure")
 @EnableConfigurationProperties(AzureStorageProperties.class)
 @RequiredArgsConstructor
 public class AzureBlobConfig {
